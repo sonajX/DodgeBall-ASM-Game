5fd084bd316d6a5c7af8bbc288f5f31738629385
samplejson <119024550+samplejson@users.noreply.github.com>
2024-05-19 14:22:30 +0800

    added border

--- test.asm
+++ test.asm
@@ -33,11 +33,11 @@
     ;   character coords
     player_x dw 160 ;x = 160, default center position
     player_y dw 100 ;y = 100, default center position
-    prev_x dw 0
-    prev_y dw 0
+    prev_x dw 160
+    prev_y dw 100
 
     ;   player size
-    player_size dw 08h  ; size of player
+    player_size dw 07h  ; size of player
     player_velocity dw 08h ; speed should be size + 1
     player_life dw 03h  ; starting hp
 
@@ -47,40 +47,48 @@
     enemy1_y dw 0
 ;   {
 
+        ; border coordinates
+    leftborder_x dw 10
+    rightborder_x dw 307
+    verticalborder_y dw 30
+    topborder_y dw 30
+    bottomborder_y dw 190
+    horizontalborder_x dw 10
+
+    ; border size
+    verticalborder_width dw 03;
+    horizontalborder_width dw 297;
+    verticalborder_height dw 163; 
+    horizontalborder_height dw 03;
+
 .code   ;   Code Segment where we write our main program
 
 Main PROC near  ;   PROC means Procedure (or Function)
-
     mov ax, @data
     mov ds, ax
-
-    call setVideoMode   ; refreshes the screen
+    call clear_screen   ; refreshes the screen
 
     ;   Display Message
-    lea dx, message_title
-    call printLine
+    
+        lea dx, message_title
+        call printLine
+        call drawBorder
 
     Check_Time:
         mov ah, 2Ch ; Set configuration for getting time
         int 21h ; CH = hour, CL = minute, DH = second, DL = 1/100 seconds
-
         cmp dh, time_aux
         JE Check_Time
-        
         mov time_aux, dl    ;   update time
+        call move_player
 
-        ;   call setVideoMode
-        call movePlayer
-        call drawPlayer
-        mov ax, player_life
-        
         ;Compare AX to 0, if player_life is 0, stop
+        mov ax, player_life
         cmp ax, 0
         JZ Stop
 
-
         JMP Check_Time
-    
+
 Stop:
     call exit
     ret
@@ -94,6 +102,13 @@ printLine proc near
 	int 21h
     ret
 printLine endp
+
+move_player proc near
+    call erasePlayer
+    call movePlayer ;if player movement is detected, inc or dec coords
+    call drawPlayer
+    ret
+move_player endp
 
 ;   For moving the pixel
 drawPlayer proc near
@@ -121,6 +136,11 @@ drawPlayer proc near
 drawPlayer endp
 
 movePlayer proc near
+    mov ax, player_x
+    mov prev_x, ax
+    mov ax, player_y
+    mov prev_y, ax
+
     ;   check if any key is being pressed (if not exit procedure)
     mov ah, 01h             ;   if no key is pressed, ZF is 0
     int 16h
@@ -179,15 +199,37 @@ movePlayer proc near
         jmp stop_move
 
     stop_move:
+        ;   mov ax, 
         ret
 
 movePlayer endp
 
 erasePlayer proc near
+    mov cx, prev_x ; CX = X, set initial x coordinates 
+    mov dx, prev_y ; DX = Y, set initial y coordinates
+    
+    ;mov prev_x
 
+    Erase_Player_Horizontal:
+        mov ah, 0Ch ;configuration to printing pixel
+        mov al, 00h ;color blue
+        mov bh, 00h ;page number (disregard)
+        int 10h ; call dos for printing pixel
+        inc cx  ; initial is cx ++, 161 0000 0000 
+        mov ax, cx
+        sub ax, prev_x
+        cmp ax, player_size      ;  ZF is -7 on first run 
+        JNE Erase_Player_Horizontal  ; !(ax > player_size)
+        mov cx, prev_x
+        inc dx
+        mov ax, dx
+        sub ax, prev_y
+        cmp ax, player_size
+        jne Erase_Player_Horizontal        
     ret
 erasePlayer endp
 ;   Clear Screen
+    
 cls proc near
 	mov ax, 0600h
 	mov bh, 07
@@ -204,7 +246,7 @@ exit proc near
     ret
 exit endp                                            ;   exit the function
 
-setVideoMode proc near
+clear_screen proc near
     ;   Set the video mode to 320x200 - mode 13h
     mov ah, 00h 
     mov al, 13h 
@@ -216,7 +258,112 @@ setVideoMode proc near
     mov bl, 00h 
     int 10h 
     ret
-setVideoMode endp
+clear_screen endp
 
+drawLeftBorder proc near
+    mov cx, leftborder_x    ; CX = X, set initial x coordinates 
+    mov dx, verticalborder_y    ; DX = Y, set initial y coordinates
+
+    ;mov prev_x
+
+    Draw_LeftBorder_Horizontal:
+        mov ah, 0Ch ;configuration to printing pixel
+        mov al, 02h ;color white
+        mov bh, 00h ;page number (disregard)
+        int 10h ; call dos for printing pixel
+        inc cx  ; initial is cx ++, 161 0000 0000 
+        mov ax, cx
+        sub ax, leftborder_x
+        cmp ax, verticalborder_width     ;  ZF is -7 on first run 
+        JNE Draw_LeftBorder_Horizontal  ; !(ax > player_size)
+        mov cx, leftborder_x
+        inc dx
+        mov ax, dx
+        sub ax, verticalborder_y
+        cmp ax, verticalborder_height
+        jne Draw_LeftBorder_Horizontal        
+    ret
+drawLeftBorder endp
+
+drawRightBorder proc near
+    mov cx, rightborder_x    ; CX = X, set initial x coordinates 
+    mov dx, verticalborder_y    ; DX = Y, set initial y coordinates
+
+    ;mov prev_x
+
+    Draw_RightBorder_Horizontal:
+        mov ah, 0Ch ;configuration to printing pixel
+        mov al, 02h ;color white
+        mov bh, 00h ;page number (disregard)
+        int 10h ; call dos for printing pixel
+        inc cx  ; initial is cx ++, 161 0000 0000 
+        mov ax, cx
+        sub ax, rightborder_x
+        cmp ax, verticalborder_width     ;  ZF is -7 on first run 
+        JNE Draw_RightBorder_Horizontal  ; !(ax > player_size)
+        mov cx, rightborder_x
+        inc dx
+        mov ax, dx
+        sub ax, verticalborder_y
+        cmp ax, verticalborder_height
+        jne Draw_RightBorder_Horizontal        
+    ret
+drawRightBorder endp
+
+drawTopBorder proc near
+    mov cx, horizontalborder_x    ; CX = X, set initial x coordinates 
+    mov dx, topborder_y           ; DX = Y, set initial y coordinates
+    ;mov prev_x
+
+    Draw_TopBorder_Horizontal:
+        mov ah, 0Ch ;configuration to printing pixel
+        mov al, 02h ;color white
+        mov bh, 00h ;page number (disregard)
+        int 10h ; call dos for printing pixel
+        inc cx  ; initial is cx ++, 161 0000 0000 
+        mov ax, cx
+        sub ax, horizontalborder_x
+        cmp ax, horizontalborder_width   ;  ZF is -7 on first run 
+        JNE Draw_TopBorder_Horizontal  ; !(ax > player_size)
+        mov cx, horizontalborder_x
+        inc dx
+        mov ax, dx
+        sub ax, topborder_y
+        cmp ax, horizontalborder_height
+        jne Draw_TopBorder_Horizontal        
+    ret
+drawTopBorder endp
+
+drawBottomBorder proc near
+    mov cx, horizontalborder_x    ; CX = X, set initial x coordinates 
+    mov dx, bottomborder_y           ; DX = Y, set initial y coordinates
+    ;mov prev_x
+
+    Draw_BottomBorder_Horizontal:
+        mov ah, 0Ch ;configuration to printing pixel
+        mov al, 02h ;color white
+        mov bh, 00h ;page number (disregard)
+        int 10h ; call dos for printing pixel
+        inc cx  ; initial is cx ++, 161 0000 0000 
+        mov ax, cx
+        sub ax, horizontalborder_x
+        cmp ax, horizontalborder_width   ;  ZF is -7 on first run 
+        JNE Draw_BottomBorder_Horizontal  ; !(ax > player_size)
+        mov cx, horizontalborder_x
+        inc dx
+        mov ax, dx
+        sub ax, bottomborder_y
+        cmp ax, horizontalborder_height
+        jne Draw_BottomBorder_Horizontal        
+    ret
+drawBottomBorder endp
+
+drawBorder proc near
+    call DrawBottomBorder
+    call DrawLeftBorder
+    call DrawRightBorder
+    call DrawTopBordeR
+    ret
+drawBorder endp
 
 end Main                                             ;   End the main function
